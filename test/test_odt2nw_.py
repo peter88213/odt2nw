10230b325f77ac849a2a0df2a2930b824f61e4e7
peter88213 <peter.88213@t-online.de>
2023-06-09 09:39:07 +0200

    Update docstrings

--- test/test_odt2nw_.py
+++ test/test_odt2nw_.py
@@ -1,7 +1,7 @@
 """Regression test for the odt2nw project.
 
 Copyright (c) 2023 Peter Triesberger
-For further information see https://github.com/peter88213/yw2nw
+For further information see https://github.com/peter88213/odt2nw
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
